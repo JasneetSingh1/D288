eb6ddfb7f88712d9d6e20482cd29224ccd5803c2
Jasneet <jasneetpalsingh619@gmail.com>
2023-11-30 22:26:15 -0700

    initial

--- src/main/java/com/example/demo/entities/StatusType.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.entities;
-
-public enum StatusType {
-    pending,ordered,cancelled ;
-}
--- src/main/java/com/example/demo/services/Checkout.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.services;
-
-public interface Checkout
-{
-    PurchaseReply placeOrder(Purchase purchase);
-}
